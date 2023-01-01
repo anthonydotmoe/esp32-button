d6ee2ba900e25987e8a55290b6c60b33c21f412d
Anthony Guerrero <anthony@anthony.moe>
2023-01-01 11:12:11 -0800

    initial commit

---
+++ include/buttonconfig.h
@@ -0,0 +1,8 @@
+#pragma once
+
+struct ButtonConfig {
+    char wifi_ssid[33];
+    char wifi_passwd[64];
+    char admin_user[16];
+    char admin_pass[64];
+};
---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <Arduino.h>
+#include <WiFiAP.h>
+#include <ESPmDNS.h>
+#include <DNSServer.h>
+#include <WebServer.h>
+#include <HTTPClient.h>
+#include <WiFiClientSecure.h>
+#include <Adafruit_NeoPixel.h>
+
+#include "buttonconfig.h"
+
+#define FORMAT_SPIFFS_IF_FAILED true
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
